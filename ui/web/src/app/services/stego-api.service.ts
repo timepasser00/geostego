2aaba6f12c3f3353723842b7df69a82264846e76
Atul Raj <atul03022019@gmail.com>
2025-12-18 09:28:21 +0530

    Added the env file

--- ui/web/src/app/services/stego-api.service.ts
+++ ui/web/src/app/services/stego-api.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class StegoApiService {
-  private baseUrl = 'http://localhost:8080';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
